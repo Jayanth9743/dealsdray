67ebc6f3f4758885fab4c2ee0e04df4bdc89ce2b
Jayanth P <jayanthjayram6968@gmail.com>
2024-10-09 13:05:29 +0530

    done changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     navigate('/login');
   };
 
-  if (loading || !admin) {
+  if (loading) {
     return <Loading />;
   }
 
@@ -45,7 +45,11 @@ const Navbar = () => {
     <div className="fixed top-0 flex flex-col items-start justify-center w-full h-24 bg-primary">
       <div className="flex items-center justify-between w-full h-[49%]">
         <p className="ml-4 text-2xl font-bold text-gray-600">DealsDray Assignment</p>
-        <p className="mr-4 text-xl font-bold text-gray-600">Welcome {admin.name}!</p>
+        {admin ? (
+          <p className="mr-4 text-xl font-bold text-gray-600">Welcome {admin.name}!</p>
+        ) : (
+          <p className="mr-4 text-xl font-bold text-gray-600">Welcome!</p>
+        )}
       </div>
       <hr className="w-full border-black border-solid" />
       <div className="flex items-center justify-around w-full text-lg font-semibold h-[49%] bg-primary">
